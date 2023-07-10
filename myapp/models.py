1d40a516a442eaa4d268a9f9efe70607f50d2328
Berdishukur <boboqulovberdishukur@gmail.com>
2023-07-10 19:30:27 +0500

    Hello

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+class Customer(models.Model):
+    first_name=models.CharField(max_length=100,null=False,blank=False)
+    last_name=models.CharField(max_length=100,null=False,blank=False)
+    phone_number=models.CharField(max_length=100,null=False,blank=False)
+    email=models.CharField(max_length=100,null=False,blank=False)
+    message=models.TextField(null=False,blank=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
+
+
+
+
+
+
+
 # # Create your models here.
 class Person(models.Model):
     first_name = models.CharField(max_length=200)
